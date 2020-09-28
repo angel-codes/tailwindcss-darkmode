7c8ecf6110056f6bb00a9162a411b61e28421769
Angel Martinez <angel.codes139@gmail.com>
2020-09-28 07:47:07 -0500

    :sparkles: feature(app): basic logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
+  // local state
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem('theme') === 'dark' ? true : false
+  );
+
+  // function to toggle dark mode
+  const handleDarkMode = e => {
+    // todo : change properties
+  };
+
   return (
     <div className="transition-colors ease-in duration-300 text-center bg-blue-100 dark:bg-blue-900">
       <header className="min-h-screen flex flex-col justify-center items-center">
@@ -15,7 +25,11 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <label className="switch">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            defaultValue={isDark}
+            onChange={handleDarkMode}
+          />
           <span className="slider round" />
         </label>
       </header>
