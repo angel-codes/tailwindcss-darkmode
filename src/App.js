2acb4b00bdd6400b4c0dc74e9a26e3c1a4843790
Angel Martinez <angel.codes139@gmail.com>
2020-09-27 23:23:17 -0500

    :sparkles: feature(App): styles for darkmode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="text-center">
+    <div className="transition-colors ease-in duration-300 text-center bg-blue-100 dark:bg-blue-900">
       <header className="min-h-screen flex flex-col justify-center items-center">
         <img
           src={logo}
@@ -11,7 +11,7 @@ function App() {
           className="animate-spin-slow pointer-events-none"
           alt="logo"
         />
-        <p className="mb-2">
+        <p className="mb-2 text-blue-900 dark:text-blue-100">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <label className="switch">
