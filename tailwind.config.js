aa91426e7ec6c8ec23f34e56813c839d19d2063b
Angel Martinez <angel.codes139@gmail.com>
2020-09-28 08:07:51 -0500

    :wrench: config(tailwindcss): generate clasess for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const plugin = require('tailwindcss/plugin');
+const selectorParser = require('postcss-selector-parser');
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -10,10 +13,29 @@ module.exports = {
         'spin-slow': 'spin 20s linear infinite'
       },
       screens: {
-        dark: { raw: '(prefers-color-schema: dark)' }
+        dark: { raw: '(prefers-color-scheme: dark)' }
       }
     }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    textColor: ['dark', 'responsive', 'hover', 'focus'],
+    backgroundColor: ['dark', 'responsive', 'hover', 'focus']
+  },
+  plugins: [
+    plugin(function ({ addVariant, prefix }) {
+      addVariant('dark', ({ modifySelectors, separator }) => {
+        modifySelectors(({ selector }) => {
+          return selectorParser(selectors => {
+            selectors.walkClasses(sel => {
+              sel.value = `dark${separator}${sel.value}`;
+              sel.parent.insertBefore(
+                sel,
+                selectorParser().astSync(prefix('.scheme-dark '))
+              );
+            });
+          }).processSync(selector);
+        });
+      });
+    })
+  ]
 };
