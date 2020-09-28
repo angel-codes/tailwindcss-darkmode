088ae03ccd67bec6ec4d5b570cfedb32b04c7dcb
Angel Martinez <angel.codes139@gmail.com>
2020-09-28 08:31:43 -0500

    :wrench: config(tailwindcss): purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
   },
-  purge: [],
+  purge: ['./src/*.js'],
   theme: {
     extend: {
       animation: {
